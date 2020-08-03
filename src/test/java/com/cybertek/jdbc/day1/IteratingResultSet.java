f6281148bc0b5f907984c0c9b33b107fa06cf155
Akbar <akbar@cybertekschool.com>
2020-08-03 12:21:34 -0400

    Iterating ResultSet using while loop

--- src/test/java/com/cybertek/jdbc/day1/IteratingResultSet.java
+++ src/test/java/com/cybertek/jdbc/day1/IteratingResultSet.java
@@ -13,17 +13,32 @@ public class IteratingResultSet {
         Statement stmnt = conn.createStatement();
         ResultSet rs   =   stmnt.executeQuery("SELECT * FROM REGIONS") ;
 
-        rs.next(); // currently we are at the first row
-        System.out.println( rs.getString("REGION_ID") + " " + rs.getString("REGION_NAME") );
+        // as long as rs.next() return true I know I have next row to print the data
+        // we will keep looping as long as rs.next() return true
 
-        rs.next();// currently we are at the second row
-        System.out.println( rs.getString(1) + " " + rs.getString(2) );
+        while (rs.next() == true ){
 
-        rs.next(); // currently we are at the third row
-        System.out.println( rs.getString("REGION_ID") + " " + rs.getString("REGION_NAME") );
+            System.out.println( rs.getString("REGION_ID") + " " + rs.getString("REGION_NAME") );
 
-        rs.next();// currently we are at the forth row
-        System.out.println( rs.getString(1) + " " + rs.getString(2) );
+        }
+
+
+
+//
+//        rs.next(); // currently we are at the first row
+//        System.out.println( rs.getString("REGION_ID") + " " + rs.getString("REGION_NAME") );
+//
+//        rs.next();// currently we are at the second row
+//        System.out.println( rs.getString(1) + " " + rs.getString(2) );
+//
+//        rs.next(); // currently we are at the third row
+//        System.out.println( rs.getString("REGION_ID") + " " + rs.getString("REGION_NAME") );
+//
+////        rs.next();// currently we are at the forth row
+////        System.out.println( rs.getString(1) + " " + rs.getString(2) );
+//
+//        System.out.println(  rs.next()   );         ;// currently we are at the forth row
+//        //System.out.println( rs.getString(1) + " " + rs.getString(2) );
 
 
 
