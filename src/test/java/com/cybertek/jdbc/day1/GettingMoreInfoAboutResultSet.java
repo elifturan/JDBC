6dc7bf912dbd50f86c4a56f0eee904068e24e7a1
Akbar <akbar@cybertekschool.com>
2020-08-03 15:35:32 -0400

    ResultSetMetadata to get the column names from the result set object

--- src/test/java/com/cybertek/jdbc/day1/GettingMoreInfoAboutResultSet.java
+++ src/test/java/com/cybertek/jdbc/day1/GettingMoreInfoAboutResultSet.java
@@ -11,7 +11,7 @@ public class GettingMoreInfoAboutResultSet {
         String password = "hr";
         Connection conn = DriverManager.getConnection(connectionStr, username, password);
         Statement stmnt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
-        ResultSet rs = stmnt.executeQuery("SELECT * FROM REGIONS");
+        ResultSet rs = stmnt.executeQuery("SELECT * FROM EMPLOYEES ");
 
         // ResultSetMetadata is data about the ResultSet like column count , column name
         // any many more info about the ResultSet itself
@@ -19,6 +19,20 @@ public class GettingMoreInfoAboutResultSet {
         // counting how many columns we have in our ResultSet object
         int columnCount =  rsmd.getColumnCount() ;
         System.out.println("columnCount = " + columnCount);
+
+        // find out column name according to the index
+        String secondColumnName = rsmd.getColumnName(2);
+        System.out.println("secondColumnName = " + secondColumnName);
+
+        // How to list all the column names from the ResultSet
+        // you know how many column we have using getColumnCount method
+        // you know how to get column name from index getColumnName method
+
+        for (int i = 1; i <= columnCount; i++) {
+            System.out.println( rsmd.getColumnName(i) ) ;
+        }
+
+
 
 
     }
